fbe8f3f5fe2b4be25160146f0cde30cf13fba316
JeffLau-byte <22123483@imail.sunway.edu.my>
2025-07-10 15:03:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 st.title("Read CSV from GitHub")
 
-url = "https://raw.githubusercontent.com/limfw/temp/main/data.csv"
+url = "https://raw.githubusercontent.com/JeffLau-byte/MachineLearning/refs/heads/main/grades.csv"
 
 def load_data():
   return pd.read.csv(url)
