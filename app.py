44db04ccf2f3fb9adc2c1da8ff8494f2aa52b96f
JeffLau-byte <22123483@imail.sunway.edu.my>
2025-07-10 15:04:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ try:
   st.success("Data loaded successfully!")
   st.dataframe(df)
 except Exception as e:
-  st.error(" Faied to Load Data :(e)")
+  st.error(" Failed to Load Data :{e}")
 
 ">> https://github.com/settings/personal-access-tokens "
 a = st.text_input("Enter a")
